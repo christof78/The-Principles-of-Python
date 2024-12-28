c38f79ce314cd99166a375e193b68094a8414ff8
christof78 <christof.king@yahoo.co.uk>
2024-12-28 13:38:31 +0000

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This is my first line of code
+print("My name is Christof King")
+print("The date is 28th December 2024")
+
+
+# In[9]:
+
+
+#Write a program to analyse real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+#find the total sales for the month
+print(a+b+c+d+e)
+#calculate the average sales price for the month
+print((a+b+c+d+e)/5)
+#find the missing value from the last column of the table
+print(c-6500)
+
+
+# In[7]:
+
+
+#Write a program to analyse real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+#find the total sales for the month
+total_sales = a+b+c+d+e
+print(total_sales)
+#calculate the average sales price for the month
+average_sales = (total_sales)/5
+print(average_sales)
+#find the missing value from the last column of the table
+property_price_c = c-6500
+print(property_price_c)
+
+
+# In[ ]:
+
+
+
+
